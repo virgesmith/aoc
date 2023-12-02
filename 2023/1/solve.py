c990c64bd1d42bc9d2fee35d9ae8831411b7d389
virgesmith <andrew@friarswood.net>
2023-12-02 12:11:38 +0000

    2023

---
+++ 2023/1/solve.py
@@ -0,0 +1,65 @@
+
+from io import StringIO
+
+import re
+
+def test1():
+    input = """\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"""
+    return StringIO(input).read().splitlines()
+
+
+def test2():
+    input = """\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+    return StringIO(input).read().splitlines()
+
+
+def live():
+    with open("2023/1/input.txt") as fd:
+        return fd.read().splitlines()
+
+
+def solve_impl(string: str, digits: dict[str, int], order: range) -> int:
+    for i in order:
+        for d in digits:
+            if string[i:].startswith(d):
+                return digits[d]
+    raise ValueError(f"no digits in {string}")
+
+
+def first(string: str, digits: dict[str, int]) -> int:
+    return solve_impl(string, digits, range(len(string)))
+
+
+def last(string: str, digits: dict[str, int]) -> int:
+    return solve_impl(string, digits, range(len(string) - 1, -1, -1))
+
+
+def solve1(strings: list[str]) -> int:
+    digits = {str(d): d for d in range(1, 10)}
+    return sum(first(s, digits) * 10 + last(s, digits) for s in strings)
+    
+
+def solve2(strings: list[str]) -> list[int]:
+    digits = {str(d): d for d in range(1, 10)} | dict(one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
+    return sum(first(s, digits) * 10 + last(s, digits) for s in strings)
+
+
+if __name__ == "__main__":
+    print(f"part 1 test = {solve1(test1())}")
+    print(f"part 1 live = {solve1(live())}")
+    print(f"part 2 test = {solve2(test2())}")
+    print(f"part 2 live = {solve2(live())}")
+
---
+++ 2023/2/solve.py
@@ -0,0 +1,61 @@
+
+from io import StringIO
+
+import re
+
+def test1():
+    input = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+    return StringIO(input).read().splitlines()
+
+
+def live():
+    with open("2023/2/input.txt") as fd:
+        return fd.read().splitlines()
+
+
+
+def solve1(strings: list[str]) -> int:
+    limits = {"red": 12, "blue": 14, "green": 13}
+    sum = 0
+    for s in strings:
+        game, outcomes = s.split(":")
+        game = int(game.replace("Game ", ""))
+        outcomes = outcomes.replace(";", ",").split(",")
+        within_limit = True
+        for dice in outcomes:
+            n, colour = dice.strip().split(" ")
+            if int(n) > limits[colour]:
+                within_limit = False
+                break
+        if within_limit:
+            sum += game
+    return sum
+    
+
+def solve2(strings: list[str]) -> list[int]:
+    sum = 0
+    for s in strings:
+        game, outcomes = s.split(":")
+        game = int(game.replace("Game ", ""))
+        outcomes = outcomes.replace(";", ",").split(",")
+        minima = dict(red=0, green=0, blue=0)
+        for dice in outcomes:
+            n, colour = dice.strip().split(" ")
+            minima[colour] = max(minima[colour], int(n))
+        sum += minima["red"] * minima["green"] * minima["blue"]
+    return sum
+
+
+if __name__ == "__main__":
+    print(f"part 1 test = {solve1(test1())}")
+    print(f"part 1 live = {solve1(live())}")
+    print(f"part 2 test = {solve2(test1())}")
+    print(f"part 2 live = {solve2(live())}")
+
+# 2575 too high
